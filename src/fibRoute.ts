5120edbc5d02d699ec9b2d441dea49cf71db51c6
Yuvanshu Agarwal <yuvanshuagarwal@yuvanshus-air.wifi.local.cmu.edu>
2023-08-30 13:32:31 -0400

    fixing fibroute6

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -40,10 +40,10 @@ import fibonacci from './fib';
 export default (req: Request, res: Response) => {
   const { num } = req.params;
 
-  const fibN: number = fibonacci(parseInt(num)); // Use `any` for now, but improve if possible
+  const fibN = fibonacci(parseInt(num)); // Use `any` for now, but improve if possible
   let result: string;
 
-  if (typeof fibN === 'number') {
+  if (fibN >= 0) {
     result = `fibonacci(${num}) is ${fibN}`;
   } else {
     result = `fibonacci(${num}) is undefined`;
