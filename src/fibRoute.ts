055ca11c95bfd4c7b0bfd526b0c14c499797fe02
Yuvanshu Agarwal <yuvanshuagarwal@yuvanshus-air.wifi.local.cmu.edu>
2023-08-30 13:19:22 -0400

    fixing fibroute4

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -15,16 +15,35 @@
 //   res.send(result);
 // };
 
-import { Request, Response } from 'express'; // Import types for Request and Response
+// import { Request, Response } from 'express'; // Import types for Request and Response
+// import fibonacci from './fib';
+
+// export default (req: Request, res: Response) => {
+//   const { num } = req.params;
+
+//   const fibN: number = fibonacci(parseInt(num));
+//   let result: string;
+
+//   if (fibN >= 0) {
+//     result = `fibonacci(${num}) is ${fibN}`;
+//   } else {
+//     result = `fibonacci(${num}) is undefined`;
+//   }
+
+//   res.send(result);
+// };
+
+
+import { Request, Response } from 'express';
 import fibonacci from './fib';
 
 export default (req: Request, res: Response) => {
   const { num } = req.params;
 
-  const fibN: number = fibonacci(parseInt(num));
+  const fibN: number = fibonacci(parseInt(num)); // Use `any` for now, but improve if possible
   let result: string;
 
-  if (fibN >= 0) {
+  if (typeof fibN === 'number') {
     result = `fibonacci(${num}) is ${fibN}`;
   } else {
     result = `fibonacci(${num}) is undefined`;
