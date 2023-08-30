56e007bc3d3ccc065954379f749065c3220087e5
Yuvanshu Agarwal <yuvanshuagarwal@yuvanshus-air.wifi.local.cmu.edu>
2023-08-30 12:58:04 -0400

    fixing fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,35 @@
-// Endpoint for querying the fibonacci numbers
+// // Endpoint for querying the fibonacci numbers
 
-import fibonacci from "./fib";
+// import fibonacci from "./fib";
 
-export default (req, res) => {
+// export default (req, res) => {
+//   const { num } = req.params;
+
+//   const fibN = fibonacci(parseInt(num));
+//   let result = `fibonacci(${num}) is ${fibN}`;
+
+//   if (fibN < 0) {
+//     result = `fibonacci(${num}) is undefined`;
+//   }
+
+//   res.send(result);
+// };
+
+import { Request, Response } from 'express'; // Import types for Request and Response
+import fibonacci from './fib';
+
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  let result: string;
 
-  if (fibN < 0) {
+  if (fibN >= 0) {
+    result = `fibonacci(${num}) is ${fibN}`;
+  } else {
     result = `fibonacci(${num}) is undefined`;
   }
 
   res.send(result);
 };
+
